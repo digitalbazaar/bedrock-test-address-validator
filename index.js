c866d19e7606e4696eec9ca2c948f989c18aaa4b
Omar Malik <omalik@vera.digitalbazaar.com>
2015-03-05 13:24:08 -0500

    Set up files for testing.

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2012-2015 Digital Bazaar, Inc. All rights reserved.
+ */
+var bedrock = require('bedrock');
+var av = require('./lib/addressValidator');
+
+// run with development defaults
+bedrock.start();
